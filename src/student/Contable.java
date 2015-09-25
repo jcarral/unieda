ff1b098c4bccc7ebcc50c566e45ce9e49cccc993
Joseba <jcarraldc@gmail.com>
2015-09-25 11:32:41 +0200

    Clase Contable iniciada

--- src/student/Contable.java
+++ src/student/Contable.java
@@ -1,6 +1,7 @@
 package student;
 
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Contable {
 
@@ -8,6 +9,7 @@ public class Contable {
 	private String falsario;
 	private int[] cantidadInicial;
 	private Iterator<Payment> itr;
+	private LinkedList[] pilaBilletes;
 	
 	public Contable(String[] jugadores, String falsario, int[] cantidadInicial, Iterator<Payment> itr){
 		
@@ -15,9 +17,49 @@ public class Contable {
 		this.falsario = falsario;
 		this.cantidadInicial = cantidadInicial;
 		this.itr = itr;
+		iniciarPila();
+		calcularPila();
 	}
+	
 	
 	public int cantidadLegalDe(String jugador){
 		return 0;
+	}
+	
+	private void iniciarPila(){
+		
+		for(int i = 0; i<jugadores.length; i++){
+		 LinkedList pila = new LinkedList<Billetes>();
+		 pila.add(new Billetes(!jugadores[i].equals(falsario), cantidadInicial[i]));
+		 
+		}
+		
+	}
+	
+	private void calcularPila(){
+		Payment pay; 
+		boolean falso;
+		while(itr.hasNext()){
+			pay = itr.next();
+			
+		}
+	}
+	
+	private int getWinnerPos(String s){
+		for(int i = 0; i<jugadores.length; i++){
+			if(jugadores[i].equals(s))
+				return i;
+		}
+		return -1;
+	}
+	private class Billetes{
+		
+		private boolean verdadero;
+		private int cantidad;
+		
+		Billetes(boolean verdadero, int cantidad){
+			this.verdadero = verdadero;
+			this.cantidad = cantidad;
+		}
 	}
 }
