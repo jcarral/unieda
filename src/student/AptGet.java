993c31bccdf5864db77a1e72dd5898328d6e787b
Joseba <jcarraldc@gmail.com>
2015-12-04 10:35:26 +0100

    Colorear tablero & apt-get

---
+++ src/student/AptGet.java
@@ -0,0 +1,25 @@
+package student;
+
+import java.util.List;
+
+/**
+ * Created by joseba on 4/12/15.
+ */
+public class AptGet {
+
+    public AptGet(String[] packages, String[][] dependecies) {
+
+    }
+
+    public List<String> installed() {
+        return null;
+    }
+
+    public List<String> install(String packageName) {
+        return null;
+    }
+
+    public List<String> purge(String pacageName) {
+        return null;
+    }
+}
--- src/student/SudokuFrame.java
+++ src/student/SudokuFrame.java
@@ -5,6 +5,8 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 /**
  * Created by joseba on 3/12/15.
@@ -15,6 +17,7 @@ public class SudokuFrame extends JFrame {
     private JTextField[][] subPanels;
 
     SudokuFrame() {
+
         super("Sudoku");
         final JPanel GuiPanel = new JPanel(new GridLayout(filas, columnas));
         subPanels = new JTextField[filas][columnas];
@@ -49,6 +52,7 @@ public class SudokuFrame extends JFrame {
                     subPanels[r][c].setBackground(new Color(0x9F9F9F));
                 else
                     subPanels[r][c].setBackground(Color.WHITE);
+                colorear(r, c);
                 GuiPanel.add(subPanels[r][c]);
 
             }
@@ -58,10 +62,17 @@ public class SudokuFrame extends JFrame {
         btnCalcular.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
                 Sudoku s1 = new Sudoku();
-                int[][] tablero = s1.resolverSudoku(getTablero());
-                setTablero(tablero);
-                setVisible(true);
+                int[][] tablero = getTablero();
+                if (!esValido(tablero))
+                    JOptionPane.showMessageDialog(null, "Tablero no valido");
+                else {
+                    tablero = s1.resolverSudoku(getTablero());
+                    setTablero(tablero);
+                }
+
+
             }
         });
 
@@ -108,7 +119,7 @@ public class SudokuFrame extends JFrame {
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
                 val = subPanels[i][j].getText();
-                if (!isNumeric(val)) {
+                if (!isNumeric(val) || (Integer.parseInt(val) > 10 || Integer.parseInt(val) < 0)) {
                     tablero[i][j] = 0;
                     continue;
                 }
@@ -120,17 +131,120 @@ public class SudokuFrame extends JFrame {
 
     public static boolean isNumeric(String str) {
         try {
-            double d = Double.parseDouble(str);
+            int i = Integer.parseInt(str);
         } catch (NumberFormatException nfe) {
             return false;
         }
         return true;
     }
 
+    private boolean esValido(int[][] tablero) {
+        boolean valido;
+        //Comprobar fila
+        for (int i = 0; i < columnas; i++) {
+            if (!comprobarFila(tablero[i]))
+                return false;
+        }
 
+        //Comprobar columna
+        for (int i = 0; i < filas; i++) {
+            if (!comprobarColumna(tablero, i))
+                return false;
+        }
+
+        //Comprobar cuadrado
+        for (int i = 0; i < filas; i += 3) {
+            for (int j = 0; j < columnas; j += 3) {
+                if (!comprobarCuadrado(tablero, i, j))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean comprobarFila(int[] linea) {
+        int[] newLinea = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        for (int i = 0; i < columnas; i++) {
+            if (linea[i] != 0)
+                if (newLinea[linea[i] - 1] != 0)
+                    return false;
+                else
+                    newLinea[linea[i] - 1] = linea[i];
+        }
+        return true;
+    }
+
+    private boolean comprobarColumna(int[][] tablero, int columna) {
+        int[] newLinea = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        for (int i = 0; i < filas; i++) {
+            if (tablero[i][columna] != 0)
+                if (newLinea[tablero[i][columna] - 1] != 0)
+                    return false;
+                else
+                    newLinea[tablero[i][columna] - 1] = tablero[i][columna];
+        }
+        return true;
+    }
+
+    private boolean comprobarCuadrado(int[][] tablero, int x, int y) {
+        int[] newLinea = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        for (int i = x; i < x + 3; i++) {
+            for (int j = y; j < y + 3; j++) {
+                if (tablero[i][j] != 0)
+                    if (newLinea[tablero[i][j] - 1] != 0)
+                        return false;
+                    else
+                        newLinea[tablero[i][j] - 1] = tablero[i][j];
+            }
+        }
+        return true;
+    }
+
+    private void colorear(int r, int c) {
+        subPanels[r][c].addFocusListener(new FocusListener() {
+            Color original;
+
+            @Override
+            public void focusGained(FocusEvent e) {
+                original = subPanels[r][c].getBackground();
+                colorearFila(c, Color.YELLOW);
+                colorearColumna(r, Color.YELLOW);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                colorearFila(c, null);
+                colorearColumna(r, null);
+            }
+        });
+
+    }
+
+    private void colorearFila(int c, Color color) {
+        boolean limpiar = (color == null);
+        for (int i = 0; i < filas; i++) {
+            if (limpiar)
+                color = getColorOriginal(i, c);
+            subPanels[i][c].setBackground(color);
+        }
+    }
+
+    private void colorearColumna(int r, Color color) {
+        boolean limpiar = (color == null);
+        for (int i = 0; i < columnas; i++) {
+            if (limpiar)
+                color = getColorOriginal(r, i);
+            subPanels[r][i].setBackground(color);
+        }
+    }
+
+    private Color getColorOriginal(int r, int c) {
+        if ((r < 3 || r > 5) && ((c < 3 || c > 5)) || ((r > 2 && r < 6) && (c > 2 && c < 6)))
+            return new Color(0x9F9F9F);
+        else
+            return Color.WHITE;
+    }
     public static void main(String[] args) {
         SudokuFrame s = new SudokuFrame();
-
-
     }
 }
