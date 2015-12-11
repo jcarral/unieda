ccfb4a78a0cee179ad5c59e77983afec9353860f
JosebaPC <jcarraldc@gmail.com>
2015-12-11 19:00:29 +0100

    Hamiltonian cycle

---
+++ src/student/HamiltonianCycleExp.java
@@ -0,0 +1,11 @@
+package student;
+
+/**
+ * Created by JosebaPC on 11/12/2015.
+ */
+public class HamiltonianCycleExp {
+    public static void main(String args[]) {
+        Hamilton obj = new Hamilton();
+        obj.getHCycle(1);
+    }
+}
